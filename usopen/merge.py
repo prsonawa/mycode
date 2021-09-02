9ffec32283987ba4a4c58fec566b1d9c3d7373f3
prsonawa <prsonawa@users.noreply.github.com>
2021-09-02 20:13:18 +0000

    Adding files to Git

---
+++ usopen/merge.py
@@ -0,0 +1,9 @@
+from napalm import get_network_driver # import code from NAPALM
+driver = get_network_driver('eos') # get the driver for Arista devices
+device = driver('sw-1', 'admin', 'alta3') # apply the switch credentials
+device.open() # start the connection
+#The next two lines of code are new
+device.load_merge_candidate("/home/student/mycode/merge.me")
+print(device.compare_config())
+device.commit_config()
+device.close()
